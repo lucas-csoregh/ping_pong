20d855ff3e2944d1187556b775ba53a3cd634691
Lucas Csöregh <lucascsoregh@protonmail.com>
2023-05-13 16:31:58 +0200

    game running

--- src/main.c
+++ src/main.c
@@ -6,17 +6,44 @@
 #include <avr/interrupt.h>
 #include <stdbool.h>
 
-bool gameOver = true;
-int velocity = 100;
-bool gameStart = false;
-bool initGame = false;
+#include <stdio.h>
+#include <string.h>
+
+int velocity = 0; // always shown on the segmented led display
+
+bool initGame = false; // intros roll
+bool gameRunning = false; // the actual game part of the program really starts here
+bool gameOver = true; // game over
+char field[] = "[--------------------------------------------------]\n";
 
 void lightsCountDown() {
+  // Blink 4 lights 
   for(int i=0; i<3; i++) {
     lightUpAllLeds();
-    _delay_ms(1000);
+    _delay_ms(250);
     lightDownAllLeds();
-    _delay_ms(1000);
+    _delay_ms(250);
+  }
+  // Blink 3 lights (+speed)
+  for(int i=0; i<3; i++) {
+    lightUpLeds(0b011100);
+    _delay_ms(250);
+    lightDownLeds(0b011100);
+    _delay_ms(250);
+  }
+  // Blink 2 lights (+speed)
+  for(int i=0; i<3; i++) {
+    lightUpLeds(0b001100);
+    _delay_ms(250);
+    lightDownLeds(0b001100);
+    _delay_ms(250);
+  }
+  // Blink 1 lights (+speed)
+  for(int i=0; i<3; i++) {
+    lightUpLeds(0b000100);
+    _delay_ms(250);
+    lightDownLeds(0b000100);
+    _delay_ms(250);
   }
 }
 void startGame(int who){
@@ -31,6 +58,8 @@ void startGame(int who){
     printf("beep noise\n"); // TODO: add oplopend geluid dat stopt wanneer het spel echt begint
 
     lightsCountDown();
+
+    gameRunning = true;
   }
 }
 
@@ -44,8 +73,6 @@ ISR(PCINT1_vect) {
   }
 }
 
-
-
 int main(void) {
   initUSART();
   initDisplay();
@@ -53,13 +80,21 @@ int main(void) {
   initButtonISR();
   enableAllLeds();
   sei();
-  while (1) {
+  while (1) { // Game loop
     int old = velocity;
     velocity = analogToDigital();
     writeNumberAndWait(velocity, 20);
     if(!initGame) {
       if(old != velocity) {
         printf("velocity: %d\n", velocity);
+      }
+    }
+    if(gameRunning) {
+      char old[50]; 
+      strcpy(old, field);
+      //printf(old);
+      if(old != field) {
+        printf(old); // the playing field = 50* "-" ( + 1* "[" and 1* "]" and 1* "\n")
       }
     }
   }
