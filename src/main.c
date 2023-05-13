627dc22081654e5658c77c108aad09dc0cd27e0f
Lucas Csöregh <lucascsoregh@protonmail.com>
2023-05-13 16:00:50 +0200

    only allow the game to be initiated once

--- src/main.c
+++ src/main.c
@@ -9,25 +9,34 @@
 bool gameOver = true;
 int velocity = 100;
 bool gameStart = false;
+bool initGame = false;
 
 
-void startGame(){
-  if(!gameStart) {
-    gameStart=true;
+void startGame(int who){
+  if(!initGame) {
+    if(who == 1) {
+      printf("Player 1 (left)\n");
+    } else if(who == 2) {
+      printf("Player 2 (right)\n");
+    }
+
+    initGame=true;
+    printf("beep noise\n"); // TODO: add oplopend geluid dat stopt wanneer het spel echt begint
   }
-  printf("beep noise\n");
 }
 
 ISR(PCINT1_vect) {
   if ((PINC & (1 << PC1)) == 0) {
-    printf("Player 1 (left)\n");
-    startGame();
+    startGame(1); // button 1 pressed, start game as player 1
   } else if ((PINC & (1 << PC2)) == 0) {
     //printf("2\n");
   } else if ((PINC & (1 << PC3)) == 0) {
-    printf("Player 2 (right)\n");
-    startGame();
+    startGame(2); // button 3 pressed, start game as player 2
   }
+}
+
+void lightsCountDown() {
+
 }
 
 int main(void) {
@@ -39,7 +48,7 @@ int main(void) {
   while (1) {
     velocity = analogToDigital();
     writeNumberAndWait(velocity, 20);
-    if(!gameStart) {
+    if(!initGame) {
       printf("velocity: %d\n", velocity);
     }
   }
