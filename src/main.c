084d268775d7c26fe6f763b0ac851513963a57e6
Lucas Csöregh <lucascsoregh@protonmail.com>
2023-05-13 17:18:03 +0200

    move ball to the right

--- src/main.c
+++ src/main.c
@@ -13,8 +13,14 @@ int velocity = 0; // always shown on the segmented led display
 
 bool initGame = false; // intros roll
 bool gameRunning = false; // the actual game part of the program really starts here
-bool gameOver = true; // game over
+bool gameOver = false ; // game over
 char field[] = "[--------------------------------------------------]\n";
+
+char line = '-';
+char ball = 'O';
+int ballIndex = 0; // where in the char field[] is the ball rn
+
+int whoHitLast =0;
 
 void lightsCountDown() {
   // Blink 4 lights 
@@ -63,15 +69,47 @@ void startGame(int who){
   }
 }
 
-ISR(PCINT1_vect) {
-  if ((PINC & (1 << PC1)) == 0) {
-    startGame(1); // button 1 pressed, start game as player 1
-  } else if ((PINC & (1 << PC2)) == 0) {
-    //printf("2\n");
-  } else if ((PINC & (1 << PC3)) == 0) {
-    startGame(2); // button 3 pressed, start game as player 2
+int rightBorder = 1;
+int leftBorder = sizeof(field) -4;
+void moveBall() {
+  if(whoHitLast == 1) { // move ball to the RIGHT (towards player2, away from player1)
+
+    field[rightBorder] = ball;
+    ballIndex = rightBorder;
+
+    while(whoHitLast == 1 && ballIndex != leftBorder) {
+      int lastIndex = ballIndex;
+      ballIndex++;
+      field[ballIndex] = ball;
+      field[lastIndex] = line;
+      printf(field); // the playing field = 50* "-" ( + 1* "[" and 1* "]" and 1* "\n")
+    }
+  } else if(whoHitLast == 2) { // move ball to the LEFT (towards player1, away from player2)
+    field[leftBorder] = ball;
   }
 }
+
+ISR(PCINT1_vect) {
+  if ((PINC & (1 << PC1)) == 0) {
+    if(!gameRunning) {
+      startGame(1); // button 1 pressed, start game as player 1
+    } else {
+      whoHitLast = 1;
+      moveBall();
+    }
+
+  } else if ((PINC & (1 << PC2)) == 0) {
+    gameOver = true;
+  } else if ((PINC & (1 << PC3)) == 0) {
+    if(!gameRunning) {
+      startGame(2); // button 3 pressed, start game as player 2
+    } else {
+      whoHitLast = 2;
+      moveBall();
+    }
+  }
+}
+
 
 int main(void) {
   initUSART();
@@ -80,7 +118,7 @@ int main(void) {
   initButtonISR();
   enableAllLeds();
   sei();
-  while (1) { // Game loop
+  while (!gameOver) { // Game loop
     int old = velocity;
     velocity = analogToDigital();
     writeNumberAndWait(velocity, 20);
@@ -89,13 +127,9 @@ int main(void) {
         printf("velocity: %d\n", velocity);
       }
     }
+    /*
     if(gameRunning) {
-      char old[50]; 
-      strcpy(old, field);
-      //printf(old);
-      if(old != field) {
-        printf(old); // the playing field = 50* "-" ( + 1* "[" and 1* "]" and 1* "\n")
-      }
-    }
+      printf(field); // the playing field = 50* "-" ( + 1* "[" and 1* "]" and 1* "\n")
+    }*/
   }
 }
